5cf4cb57556f752a714a5f99a3a7f0a24a4f820c
dimapod <Sonrisa50>
2014-01-10 18:51:26 +0100

    Clean up

--- server.js
+++ server.js
@@ -1,41 +1,23 @@
-
 var sio     = require('socket.io');
 var http    = require('http');
-//var content = require('node-static');
 var cmd     = require('./lib/command').cmd;
 var g       = require('./lib/common');
-var express = require('express')
-var path = require('path');
+var express = require('express');
+var path    = require('path');
 
 g.log('Starting Graffeine server');
-
-//var file = new(content.Server)('public');
-//
-//var handler = function (request, response) {
-//    request.addListener('end', function () {
-//        file.serve(request, response);
-//    });
-//}
-
-//var srv = http.createServer(handler);
-//srv.listen(g.config.server.port);
 
 var app = express();
 
 app.configure(function(){
-    app.set('port', process.env.PORT || 7006);
+    app.set('port', g.config.server.port || 7006);
     app.use(express.static(path.join(__dirname, './public')));
 });
 
-var srv = http.createServer(app).listen(app.get('port'), function(){
-    console.log("Express server listening on port " + app.get('port'));
+var ws = http.createServer(app).listen(app.get('port'), function(){
+    g.log('Express server listening on port ' + g.config.server.port);
 });
 
-
-//g.log('Open browser to http://127.0.0.1:' + g.config.server.port);
-//g.log('Starting WS server on ' + g.config.server.port);
-
-var ws = srv.listen(g.config.server.port);
 var conn = sio.listen(ws, { log: true });
 
 conn.sockets.on('connection', function (socket) {
